9e7ee18f6b5009abdcf3852a086e57f1e7b5425c
Greg Morgan <gregm9999@gmail.com>
2019-01-08 22:54:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 /* Your code goes here */
+
+var customerName="bob";
+var bestCustomer;
+const leastFavorite="joe"
+
+function upperCaseCustomerName() {
+  customerName=customerName.toUpperCase();
+}
+
+function setBestCustomer () {
+  bestCustomer="not bob";
+}
+
+function overWriteBestCustomer() {
+  bestCustomer="maybe bob";
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavorite="jane";
+}
+
+function attemptTwoFavoriteCustomers() {
+  const favoriteCustomer="jim"
+  const favoriteCustomer="adam"
+}
